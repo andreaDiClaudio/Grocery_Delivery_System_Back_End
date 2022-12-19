7d6818581c6b0f3c7b35c2dfd522c87f0cd1d1b3
andreaDiClaudio <andreadicla@gmail.com>
2022-12-19 09:10:29 +0100

    First Commit

---
+++ src/main/java/com/example/examprogramminggrocerydeliverysystembackend/ExamProgrammingGroceryDeliverySystemBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.example.examprogramminggrocerydeliverysystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProgrammingGroceryDeliverySystemBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProgrammingGroceryDeliverySystemBackEndApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/com/example/examprogramminggrocerydeliverysystembackend/ExamProgrammingGroceryDeliverySystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examprogramminggrocerydeliverysystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProgrammingGroceryDeliverySystemBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
